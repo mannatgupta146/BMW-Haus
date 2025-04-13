11ba85a0870b9b82c8109060a25b93335578449a
mannatgupta146 <mannatgupta146@gmail.com>
2025-04-14 01:51:25 +0530

    added

--- script.js
+++ script.js
@@ -2,3 +2,38 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function firstPageAnim() {
+    var tl = gsap.timeline();
+  
+    tl.from("#nav", {
+      y: "-10",
+      opacity: 0,
+      duration: 1.5,
+      ease: Expo.easeInOut,
+    })
+      .to(".boundingelem", {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        delay: -1,
+        stagger: 0.2,
+      })
+      .from("#homefooter", {
+        y: -10,
+        opacity: 0,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut,
+      });
+  }
+
+
+function circleMouseFollower(){
+    window.addEventListener('mousemove', function(dets){
+        document.querySelector('#circlepointer').style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+})
+}
+
+circleMouseFollower();
+firstPageAnim();
